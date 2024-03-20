64680fac62d048a08608703f029ab67f704d3b21
3ralon <albperleo@alum.us.es>
2024-03-20 21:29:18 +0100

    exiting handler

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ TOKEN_FILE = 'git.token'
 ORGANISATION_NAME = 'Aparking'
 REPOSITORY_NAME = 'AparKing_Backend'
 
-token_path = WORKSPACE_ROOT / TOKEN_FILE
-if token_path.exists():
-    with open(token_path, 'r') as f:
-        token = f.read()
-else:
-    print("git.token file does not exist")
+def main():
 
-github = Github(token)
+    token_path = WORKSPACE_ROOT / TOKEN_FILE
+    if token_path.exists():
+        with open(token_path, 'r') as f:
+            token = f.read()
+    else:
+        print("git.token file does not exist")
 
-# Retrieve repository
-organisation = github.get_organization(ORGANISATION_NAME)   
-repository = organisation.get_repo(REPOSITORY_NAME)
+    github = Github(token)
 
-milestones = repository.get_milestones(state='all')
-issues = repository.get_issues(state='all', milestone=milestones[0])
+    # Retrieve repository
+    organisation = github.get_organization(ORGANISATION_NAME)   
+    repository = organisation.get_repo(REPOSITORY_NAME)
 
-for issue in issues:
-    for l in issue.get_labels():
-        print(l.raw_data)
+    milestones = repository.get_milestones(state='all')
+    issues = repository.get_issues(state='all', milestone=milestones[0])
+
+    for issue in issues:
+        print(issue.labels)
+
+if __name__ == '__main__':
+    import sys
+    
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Exiting...')
+        sys.exit(0)
